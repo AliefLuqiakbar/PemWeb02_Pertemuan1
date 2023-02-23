3ad86ab455b35db9e058617d3d563e23f60ed237
AliefLuqiakbar <aliefluqiakbar19@gmail.com>
2023-02-23 18:00:56 +0700

    Menyambungkan Git Hub

---
+++ tipedata.php
@@ -0,0 +1,23 @@
+<?php
+
+// Integer
+$usia = 19;
+
+// Float
+$phi = 3.14;
+
+// String
+$kemerdekaan = "17 Agustus 1945"
+
+// Boolean
+$isStudent = true;
+
+// Array
+$mhs1 = ["Alief", 0110222171, "Bogor"];
+
+// Array Asosiatif
+$mhs1 = [
+    "Nama" => "Alief"
+    "NIM" => 0110222171
+    "Asal" => "Bogor"
+];
---
+++ variable.php
@@ -0,0 +1,7 @@
+<?php
+$nama = "Alief";
+$usia = 19;
+
+$usia_5_mendatang = $usia + 5;
+
+echo "Hai, nama saya $nama";
